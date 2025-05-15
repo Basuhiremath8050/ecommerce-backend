2b493b614ee0fec2e8110d756fb41d2177342b13
Basavarj Hiremath <basavarajhiremath@nextiqz.com>
2025-05-15 23:27:58 +0530

    Added JwtService and JwtAuthenticationFilter

---
+++ user-service/src/main/java/com/ecommerce/userservice/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.ecommerce.userservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Autowired
+//    private JwtAuthenticationFilter jwtAuthFilter;
+}
