714298373fc79965bbacf3bc1aeccfded6626731
Basuhiremath8050 <basuhiremat931@gmail.com>
2025-07-25 23:44:31 +0530

    kafka config error

--- user-service/src/main/java/com/ecommerce/userservice/feignclient/ProductClient.java
+++ user-service/src/main/java/com/ecommerce/userservice/feignclient/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service", configuration = FeignClientConfig.class)
 public interface ProductClient {
 @GetMapping("/api/products/{id}")
 ProductDto getProductById(@PathVariable("id") Long productId);
